2f8b6c551131093d02235ef8123bd22d780de502
chyj4747 <chyj4747@163.com>
2016-02-22 16:55:14 -0500

    index time counter

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -8,6 +8,31 @@ class IndexController extends Controller {
         //L()获得全部语言,L(username)获得指定语言
         //$this->assign('lang',L());//把语言值传进页面
 
+        $news = array(
+            "2016年春节期间AME速运服务公告",
+            "关于我国近期中东部地区异常天气...",
+            "关于2016年国际件运费调整的通知",
+            "关于12月19日到12月22日期间...",
+            "关于第十四次上海合作组织政府首...",
+            "关于\"11.11\"期间电商快件收派...",
+            "加币汇率影响下，物流费用的调整...",
+            "AME运费人民币折算方法（2016...",
+            "AME美洲快递网站系统维护通知...",
+            "中国大陆检验检疫：关于7个品牌...",
+            "AME\"国际特惠\"升级 跨境出口价"
+        );
+        $activities = array(
+            array("title"=>"americomall网上下单免运费", "end_time"=>"1456254969"),
+            array("title"=>"PBN网上下单满100运费5折", "end_time"=>"1456341363"),
+            array("title"=>"春节特价无首磅", "end_time"=>"1456254968"),
+            array("title"=>"保健品2.99/磅超低价", "end_time"=>"1456427768"),
+            array("title"=>"下载AME手机端APP，赢大奖", "end_time"=>"1456427755")
+        );
+        $cur_time = time();
+        $this->assign("news", $news);
+        $this->assign("activities", $activities);
+        $this->assign("cur_time", $cur_time);
+
         $this->display();
     }
 }
---
+++ App/Home/View/Default/js/common.js
@@ -0,0 +1,82 @@
+(function($) {
+    $.extend({
+        myTime: {
+            /**
+             * 当前时间戳
+             * @return {int}        unix时间戳(秒)
+             */
+            CurTime: function(){
+                return Date.parse(new Date())/1000;
+            },
+            /**
+             * 日期 转换为 Unix时间戳
+             * @param string 2014-01-01 20:20:20  日期格式
+             * @return         unix时间戳(秒)
+             */
+            DateToUnix: function(string) {
+                var f = string.split(' ', 2);
+                var d = (f[0] ? f[0] : '').split('-', 3);
+                var t = (f[1] ? f[1] : '').split(':', 3);
+                return (new Date(
+                        parseInt(d[0], 10) || null,
+                        (parseInt(d[1], 10) || 1) - 1,
+                        parseInt(d[2], 10) || null,
+                        parseInt(t[0], 10) || null,
+                        parseInt(t[1], 10) || null,
+                        parseInt(t[2], 10) || null
+                    )).getTime() / 1000;
+            },
+            /**
+             * 时间戳转换日期
+             * @param unixTime    待时间戳(秒)
+             * @param type        "date"或"time"或"all"
+             * @param timeZone    时区
+             * @return {Array}
+             */
+            UnixToDate: function(unixTime, type, timeZone) {
+                if (typeof (timeZone) == 'number')
+                {
+                    unixTime = parseInt(unixTime) + parseInt(timeZone) * 60 * 60;
+                }
+                var time = new Date(unixTime * 1000);
+                var ymdhis = [];
+                if (type != "time") {
+                    ymdhis['year'] = time.getUTCFullYear();
+                    ymdhis['month'] = (time.getUTCMonth() + 1);
+                    ymdhis['day'] = time.getUTCDate();
+                }
+                if (type != "date")
+                {
+                    ymdhis['hour'] = time.getUTCHours();
+                    ymdhis['minute'] = time.getUTCMinutes();
+                    ymdhis['second'] = time.getUTCSeconds();
+                }
+                return ymdhis;
+            },
+
+            /**
+             * 根据时间戳计算剩余时间，返回剩余时间数组，最大值小时
+             * @param start
+             * @param end
+             * @constructor
+             * @return {Array}
+             */
+            TimeRemain: function(start, end) {
+                var result = [];
+                start = Number(start);
+                end = Number(end);
+                if (end > start) {
+                    var delta = end - start;
+                    var hour = parseInt(delta / 3600);
+                    delta -= 3600 * hour;
+                    var minute = parseInt(delta / 60);
+                    var second = delta - minute * 60;
+                    result['hour'] = hour;
+                    result['minute'] = minute;
+                    result['second'] = second;
+                }
+                return result;
+            }
+        }
+    });
+})(jQuery);
